df80be2335465e88c41597bb4bbdfafe778c21ef
Anas AIT AOMAR <56308112+AnasAito@users.noreply.github.com>
2021-05-04 00:56:15 +0000

    add frontend boiler-plate

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="bg-red-400 h-96 w-96">geomviz</div>
+    </div>
+  );
+}
+
+export default App;
