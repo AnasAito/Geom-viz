d1b347b742adee607a6e5c3d002fdbe0d9faacef
AnasAito <khdimaty@gmail.com>
2021-05-06 13:50:55 +0000

    add charting lib with event selct

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,54 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Plot, { Plotly } from "react-plotly.js";
 import Heading from "./layouts/heading";
 function App() {
+  var config = {
+    displaylogo: false,
+    responsive: true,
+    modeBarButtonsToAdd: [
+      {
+        name: "button1",
+        // icon: Plotly.Icons.pencil,
+        direction: "up",
+        click: function (gd) {
+          alert("button1");
+        },
+      },
+    ],
+    modeBarButtonsToRemove: [
+      "hoverCompareCartesian",
+      "hoverClosestCartesian",
+      "zoom2d",
+      "pan2d",
+      // "select2d",
+      "lasso2d",
+      "zoomIn2d",
+      "zoomOut2d",
+      "autoScale2d",
+      "resetScale2d",
+      "toggleSpikelines",
+    ],
+  };
   return (
     <div className="App">
       <Heading />
+      <Plot
+        config={config}
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: "scatter",
+            mode: "markers",
+            marker: { color: "red" },
+          },
+        ]}
+        layout={{ title: "scatter Plot" }}
+        onSelected={(props) =>
+          console.log(props.points.map((point) => [point.x, point.y]))
+        }
+      />
     </div>
   );
 }
